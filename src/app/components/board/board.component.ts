ac1599041f9de4093b593e7ee9fe58bbf5e55a10
futoruma <evgeny.martov@gmail.com>
2023-01-14 23:06:25 +0900

    Added data binding.

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -3,15 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
 export class BoardComponent implements OnInit {
-  board: Array<Number> = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-  // 0 - empty, 1 - marked by 'X', 2 - marked by 'O'
+  board: Array<number> = [2, 2, 2, 2, 2, 2, 2, 2, 2];
+  // 0 - 'X', 1 - 'O', 2 - empty
+  curPlayer: number = 0;
+  curMove: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onCellClick(index: number) {
+    if (this.board[index] < 2) {
+      return;
+    }
+    this.board[index] = this.curPlayer;
+    this.curPlayer ^= 1;
+    this.curMove++;
   }
-
 }
--- src/app/components/cell/cell.component.ts
+++ src/app/components/cell/cell.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-cell',
@@ -7,10 +7,15 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CellComponent implements OnInit {
   @Input() cell: Number = 0;
+  @Output() onClickEvent = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick() {
+    this.onClickEvent.emit();
   }
 
 }
